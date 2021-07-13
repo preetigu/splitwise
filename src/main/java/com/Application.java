aa302a559a573a7eebf6ebaf33a2900b747768d8
Preeti Gupta <preeti.in@gmail.com>
2021-07-13 12:34:44 +0530

    new change

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-// Test spring boot application
+// new change: Test spring boot application
 @SpringBootApplication
 @EntityScan(basePackages = {"com.application.*"} )
 @EnableJpaRepositories(basePackages = {"com.application.*"})
